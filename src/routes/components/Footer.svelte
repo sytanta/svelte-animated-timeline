10fbb0769914522efe77153c447b3b33ecda17a5
TA SY TAN <sytanta@yahoo.com>
2023-02-20 17:33:21 +0700

    Update footer github link

--- src/routes/components/Footer.svelte
+++ src/routes/components/Footer.svelte
@@ -1,8 +1,18 @@
-<footer>Github</footer>
+<footer>
+	<a
+		href="https://github.com/sytanta/svelte-animated-timeline"
+		target="_blank"
+		rel="noreferrer noopener">Github</a
+	>
+</footer>
 
 <style>
 	footer {
 		padding: 2rem;
 		text-align: center;
+	}
+
+	a {
+		text-decoration: none;
 	}
 </style>
